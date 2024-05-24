59d8d1847ddd57d68af5ff89a4b8ecbb1f55547c
Andrenov Ruslan <ruslan.andrenov2003@gmail.com>
2024-05-25 00:29:23 +0800

    add component pagination

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,3 +1,4 @@
+import { Pagination } from '@mui/material';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -89,6 +90,7 @@ function MainContent(props) {
 							/>
 						))}
 					</ul>
+					<Pagination count={10} shape='rounded' />
 				</div>
 			</div>
 		</>
