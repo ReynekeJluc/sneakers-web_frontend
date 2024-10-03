ce3fe16d0f8304229d6616d16a2beaf4d7d1ee37
Reinecke_Fox <72546226+ReynekeJluc@users.noreply.github.com>
2024-10-03 17:20:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { fetchAuthMe } from './redux/slices/auth.jsx';
 
